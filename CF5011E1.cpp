61291392d0b0a14257a2fae04349cd67ecf1db70
AndrewWayne <AndrewWayne2016@gmail.com>
2018-10-30 10:12:46 +0800

    整理

--- CF5011E1.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <iostream>
-using namespace std;
-int n,m,
-int main(){
-
-  return 0;
-}
--- D.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <iostream>
-using namespace std;
-int n,a,b;
-int main(){
-  cin>>n;
-
-  return 0;
-}
