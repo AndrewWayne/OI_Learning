69487715a98dfcac1c2b7fc2fe63858b893aa44b
AndrewWayne <AndrewWayne2016@gmail.com>
2019-03-31 11:49:01 +0800

    2019.3.30Luogu 2327

--- P2327.cpp
+++ P2327.cpp
@@ -3,22 +3,32 @@
 using namespace std;
 const int maxn = 1e4 + 10;
 int n, a[maxn], f[maxn][8];
-bool dfs(int at, int stat){
+bool visit[maxn][8];
+int dfs(int at, int stat){
+    if(visit[at][stat])
+        return f[at][stat];
+    visit[at][stat] = true;
     int val = a[at];
     for(int i = 0; i <= 2; i++)
         val -= (stat >> i) &1;
-    if(val > 2 || val < 0)
-        return false;
-    int temp = (stat << 1) &7;
-    for(int i = 0; i <= val; i++){
-        
-    }
+    if(val > 1 || val < 0)
+        return f[at][stat] = 0;
+    if(at == n && val == 0)
+        return f[at][stat] = 1;
+
+    if(val == 0)
+        f[at][stat] += dfs(at+1, (stat << 1)&7);
+    if(val == 1)
+        f[at][stat] += dfs(at+1, ((stat|1) << 1)&7);
+
+    return f[at][stat];
 
 }
 int main(){
     cin >> n;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
-    dfs(1, 0);
+
+    cout << dfs(1, 0) + dfs(1, 2) << endl;
     return 0;
 }
