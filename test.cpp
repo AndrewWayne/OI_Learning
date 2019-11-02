d3d875da641a33348b2740d93e24a5fc2d879fd6
AndrewWayne <AndrewWayne2016@gmail.com>
2019-11-02 11:35:31 +0800

    clear

--- test.cpp
+++ test.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-	freopen("a.in", "r", stdin);
+	freopen("a.in", "w", stdout);
+	puts("10");
+	int n = 10;
+	for(int i = 1; i <= 10; i++){
+		printf("%d ", rand()%10 + 1);
+	}
 	return 0;
 }
